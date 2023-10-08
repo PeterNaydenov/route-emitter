ea1c2c973700ab9f2043ac0d00679299efaf34dd
Peter Naydenov <peter.naydenov@gmail.com>
2023-10-08 20:50:30 +0300

    Types for method 'createURL'

--- src/methods/createURL.js
+++ src/methods/createURL.js
@@ -1,4 +1,10 @@
 function createURL ( dependencies, state ) {
+/**
+ * @function createURL
+ * @description Creates URL from address name and data
+ * @param {string} addressName - Name of the address
+ * @param {object|string} data - Data to be used for creating URL(optional)
+ */
 return function createURL ( addressName, data={} ) {
         const { routes } = state;
         if ( !routes[addressName] ) {  // If address is not registered
