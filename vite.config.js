56f1f7816e433765598b13dc73a84257a67cd3d4
Peter Naydenov <peter.naydenov@gmail.com>
2026-04-03 13:44:16 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-import rollupOptions from './rollup.config.js'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build : {
-            rollupOptions
+  build: {
+    lib: {
+      entry: 'src/main.js',
+      name: 'routeEmitter',
+      fileName: (format) => `route-emitter.${format === 'es' ? 'esm' : format}.js`
+    },
+    rollupOptions: {
+      external: ['ask-for-promise', 'url-pattern', '@peter.naydenov/notice'],
+      output: {
+        globals: {
+          'ask-for-promise': 'askForPromise',
+          'url-pattern': 'UrlPattern',
+          '@peter.naydenov/notice': 'notice'
         }
+      }
+    }
+  }
 })
